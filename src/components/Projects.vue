5966c7b05d44ac3cb26d26546d85753ab55f64cb
Halldor Valur Stefansson <doristef@gmail.com>
2019-04-19 14:49:08 +0200

    Styled Projects page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,7 +10,8 @@
           </div>
 <!-- Projects - Noroff year 1 -->                     
           <div class="[ ml-4 mr-2 mt-4 ]">
-            <h2>First Year: </h2>
+              <h2>Norofff - 1st Year</h2>
+              <div class="[ pl-2 pl-lg-5 ]">
                 <h4>First Semester, 2017: </h4>
                 <ul>
                     <li><a href="http://doristef.me/semester2/Portfolio1/RedPepperPizza/RedPepperPizza_OLD.pdf" alt="First Semester MoodBoard, 2017"><b>PDF:</b> MoodBoard</a></li>
@@ -25,6 +26,7 @@
                     <li><a href="http://doristef.me/semester2/Portfolio1/recycle/" alt="Remake, Re-Cycle Website, Semester 2, 2018">Remake, Re-Cycle Website</a></li>
                     <li><a href="http://doristef.me/spacex/" alt="Second Semester, Project Ecxam, Semester 2, 2018">SpaceX Website, Project Exam</a></li>
                 </ul>
+            </div>
           </div>
 
       </b-container>
@@ -35,22 +37,24 @@
       <b-container>
 
 <!-- Projects - Noroff year 2 -->               
-          <div class="[ ml-4 mr-2 mt-4 ]">
-            <h2>Second Year: </h2>
-                <h4>Third Semester, 2018: </h4>
-                <ul>
-                    <li><a href="http://doristef.me/HomeDecor/" alt="Home Decor, Website, Semester 3, 2018">Home Decor, Website </a></li>
-                    <li><a href="http://doristef.me/HomeDecor2/shop.html" alt="Home Decor 2, Website / Shop Section, Semester 3, 2018">Home Decor, Website / Shop Section </a></li>
-                    <li><a href="http://doristef.me/HomeDecor3/shop.html" alt="Home Decor 3, Website / Shop Section, Semester 3, 2018">Home Decor, Website / Shop Section v2</a></li>
-                    <li><a href="http://doristef.me/Semester_Project2" alt="Shields & Snakes - BoardGame, Semester Project, Semester 3, 2018">Shields & Snakes - JavaScript BoardGame, Semester Project</a></li>
-                </ul>
-                
-                <h4>Fourth Semester, 2019: </h4>
-                <ul>
-                    <li><a href="http://doristef.me/semester4/noroff-reactapp/#/" alt="Using Rick&Morty API, Semester 4, 2019">Rick & Morty API - Character Cards, React </a></li>
-                    <li><a href="http://doristef.me/semester4/noroff-vueapp/" alt="Using RecipePuppy API, Semester 4, 2019">RecipePuppy API, Vue </a></li>
-                    <li><a href="http://doristef.me/semester4/noroff-angularapp/" alt="Dashboard w/ multiple APIs, Semester 4, 2019">Dashboard - Multiple APIs, Angular --> Username: halldor, Password: doristef </a></li>
-                </ul>
+          <div class="[ ml-4 mr-2 mt-4 ][ container-color-content ]">
+              <h2>Norofff - 2nd Year</h2>
+              <div class="[ pl-2 pl-lg-5 ]">
+                    <h4>Third Semester, 2018: </h4>
+                    <ul>
+                        <li><a class="[ container-color-link ai-element ai-element_type2 ai-element3 ]" href="http://doristef.me/HomeDecor/" alt="Home Decor, Website, Semester 3, 2018">Home Decor, Website </a></li>
+                        <li><a class="[ container-color-link ]" href="http://doristef.me/HomeDecor2/shop.html" alt="Home Decor 2, Website / Shop Section, Semester 3, 2018">Home Decor, Website / Shop Section </a></li>
+                        <li><a class="[ container-color-link ]" href="http://doristef.me/HomeDecor3/shop.html" alt="Home Decor 3, Website / Shop Section, Semester 3, 2018">Home Decor, Website / Shop Section v2</a></li>
+                        <li><a class="[ container-color-link ]" href="http://doristef.me/Semester_Project2" alt="Shields &amp; Snakes - BoardGame, Semester Project, Semester 3, 2018">Shields &amp; Snakes - JavaScript BoardGame, Semester Project</a></li>
+                    </ul>
+                    
+                    <h4>Fourth Semester, 2019: </h4>
+                    <ul>
+                        <li><a class="[ container-color-link ]" href="http://doristef.me/semester4/noroff-reactapp/#/" alt="Using Rick&amp;Morty API, Semester 4, 2019">Rick &amp; Morty API - Character Cards, React </a></li>
+                        <li><a class="[ container-color-link ]" href="http://doristef.me/semester4/noroff-vueapp/" alt="Using RecipePuppy API, Semester 4, 2019">RecipePuppy API, Vue </a></li>
+                        <li><a class="[ container-color-link ]" href="http://doristef.me/semester4/noroff-angularapp/" alt="Dashboard w/ multiple APIs, Semester 4, 2019">Dashboard - Multiple APIs, Angular --> Username: halldor, Password: doristef </a></li>
+                    </ul>
+                </div>
           </div>
 
       </b-container>
